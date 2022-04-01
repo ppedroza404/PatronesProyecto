8660bd53ba3097592482713aa1f7d697fab05a8e
Pablo <ppedrozas@ucenfotec.ac.cr>
2022-03-31 22:53:29 -0600

    Setup

---
+++ src/main/java/com/cenfotech/proyectopatrones/ProyectoPatronesApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotech.proyectopatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPatronesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoPatronesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cenfotech/proyectopatrones/ProyectoPatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotech.proyectopatrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPatronesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
