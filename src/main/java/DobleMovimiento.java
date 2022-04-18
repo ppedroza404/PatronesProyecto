f45b75641c0527bd33dde5c7150088450108b9d6
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:52:43 -0600

    Delete DobleMovimiento.java

--- src/main/java/DobleMovimiento.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import decorador.decorador.AtaqueEspecial;
-
-public class DobleMovimiento extends AtaqueEspecial {
-    public DobleMovimiento(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
