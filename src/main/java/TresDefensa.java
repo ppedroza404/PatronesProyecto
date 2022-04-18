8312b87286510f8be3d30f3a00b85ac10b153282
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:47:10 -0600

    Delete TresDefensa.java

--- src/main/java/TresDefensa.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import com.proyecto.Decoradores.AtaqueEspecial;
-
-public class TresDefensa extends AtaqueEspecial {
-    public TresDefensa(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
