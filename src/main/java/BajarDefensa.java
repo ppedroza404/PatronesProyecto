5abe25a06cb8ed6f229fe4f677f8d5b9def4bfc2
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:52:02 -0600

    Delete BajarDefensa.java

--- src/main/java/BajarDefensa.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import decorador.decorador.AtaqueEspecial;
-
-public class BajarDefensa extends AtaqueEspecial {
-    public BajarDefensa(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
