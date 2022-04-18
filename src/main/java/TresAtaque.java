1755d0c4fda10d3e10717bb310e9a370f47c566a
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:53:46 -0600

    Delete TresAtaque.java

--- src/main/java/TresAtaque.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import decorador.decorador.AtaqueEspecial;
-
-public class TresAtaque extends AtaqueEspecial {
-    public TresAtaque(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
