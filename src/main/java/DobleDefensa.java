275114810f42f8948d45f891894497d153d1cc35
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:52:33 -0600

    Delete DobleDefensa.java

--- src/main/java/DobleDefensa.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import decorador.decorador.AtaqueEspecial;
-
-public class DobleDefensa extends AtaqueEspecial {
-    public DobleDefensa(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
