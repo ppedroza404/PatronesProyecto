6710cca26d6c158dee964b2e5631ae2949b3a629
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:52:53 -0600

    Delete DosCasillas.java

--- src/main/java/DosCasillas.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import com.proyecto.Decoradores.AtaqueEspecial;
-
-public class DosCasillas extends AtaqueEspecial {
-    public DosCasillas(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
