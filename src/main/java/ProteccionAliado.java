fa36e63dd9cf93881d12abb53b1d05917a97af93
felipema23 <99449137+felipema23@users.noreply.github.com>
2022-04-17 23:53:29 -0600

    Delete ProteccionAliado.java

--- src/main/java/ProteccionAliado.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.AtaquesEspeciales;
-
-import decorador.decorador.AtaqueEspecial;
-
-public class ProteccionAliado extends AtaqueEspecial {
-    public ProteccionAliado(String nombre, String funcion) {
-        super(nombre, funcion);
-    }
-}
